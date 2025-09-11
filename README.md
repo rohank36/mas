9775c91446cb6c0aa4a57bc0b12369c017bd9735
Rohan Kanti <rohan.kanti@mail.mcgill.ca>
2025-09-11 00:54:11 -0400

    werk

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 ai swarm
+
+goal: make money
+
+- what is the highest leverage task an agent can do? 
+- Answer: make money in public markets
+    - how would you do this?
+    - stock price reflects market sentiment. people determine how they feel about a stock based on information they have
+    - thus information is the key driver for a stock price.
+    - llms have all the information in the internet and the ability to find new information.
+    - the question becomes whether it can find the right information.
+        - whether the swarm can find the right niche information, piece it together, and find patterns
+    - input to the model: a public company and it's current stock price. output: the forecasted stock price + the swarm's evidence/reasoning. 
+
+find the center and watch them
+the center is the economy. individuals + market forces != economy, center + debt relationships = economy. 
+the center adjusts the obligations it enforces
+When the center shifts terms the entire system reorients instantly
+If you want to understand (or profit from) markets, you study the center - who it is, what obligations it enforces, how its rituals are evolving—not the surface noise.
+    - Will Manidis: the "markets" are ritualized social networks of a very small number of live players/market participants. 
---
+++ test.py
@@ -0,0 +1,51 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()    
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+tools = [
+    {
+        "type": "function",
+        "name": "get_weather",
+        "description": "Get current temperature for a given location.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "location": {
+                    "type": "string",
+                    "description": "City and country e.g. Toronto, Canada",
+                }
+            },
+            "required": ["location"],
+            "additionalProperties": False,
+        },
+        "strict": True,
+    },
+    """
+    {
+        "type": "web_search_preview",
+        "search_context_size": "medium" # amount of context space to use for the search. low, medium, high. 
+    }
+    """
+]
+
+response = client.responses.create(
+    model="gpt-5-mini",
+    include = ["web_search_call.action.sources"],
+    parallel_tool_calls = True,
+    reasoning={"effort": "medium", "summary":"auto"}, # effort = low, medium, high. summary = auto, concise, detailed
+    temperature = 1, # [0,2]. 1 is stable. < 1 samples more likely tokens. > 1 increases probability of sampling lower likelihood next tokens
+    text = {"verbosity":"medium"}, # low, medium, high
+    tools = tools,
+    tool_choice = "auto", # none, auto, required
+    
+    input="What is the weather in paris right now?",
+
+
+
+)
+
+print(response)
+
